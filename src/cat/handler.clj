0ed4754cbbf0fb8915024d6d87a4e21cd986636b
Yuki Nagae <yuki.nagae1130@gmail.com>
2015-08-10 00:25:15 +1000

    implement simple cat function
    only 3 responses: zzz, meow, and mew

--- src/cat/handler.clj
+++ src/cat/handler.clj
@@ -1,13 +1,15 @@
 (ns cat.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.response :as response]))
 
 (defn simple [] "meow")
 
 (defroutes app-routes
-  (GET "/" [] "zzz")
-  (GET "/:call" [call] (simple))
+  (GET "/" [] (response/redirect "index.html"))
+  (GET "/call/" [] "zzz")
+  (GET "/call/:call" [call] (simple))
   (route/not-found "mew"))
 
 (def app
