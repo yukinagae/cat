1f37888c8fbafe803b838c8db9a82f67d84a91b4
Yuki Nagae <yuki.nagae1130@gmail.com>
2015-08-11 11:45:34 +1000

    get quotes from external source using enlive
    just a example and some parts are buggy or insufficient

--- src/cat/engine/linus.clj
+++ src/cat/engine/linus.clj
@@ -1,3 +1,15 @@
-(ns cat.engine.linus)
+(ns cat.engine.linus
+  (:require [net.cgrand.enlive-html :as html]))
 
-(defn response [] "Talk is cheap. Show me the code. ― Linus Torvalds")
+;; TODO this is buggy. should avoid "External Links"
+(defn quotes []
+  (-> (java.net.URL. "https://en.wikiquote.org/wiki/Linus_Torvalds") 
+    html/html-resource 
+    (html/select [:div#mw-content-text :> :ul :> :li]))) 
+
+;; TODO 0 should be random depends on quotes size
+(defn get-quote [quotes]
+  (-> (nth quotes 0) :content first))
+
+;; "Talk is cheap. Show me the code. ― Linus Torvalds"
+(defn response [] (-> (quotes) get-quote))
